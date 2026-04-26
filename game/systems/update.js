475b85d8da66378bd6093e713783f99e2632ea16
itsbloxonyt <phamhoanglam7749@gmail.com>
2026-04-26 17:00:20 +0700

    i forgot the } again

--- game/systems/update.js
+++ game/systems/update.js
@@ -147,7 +147,7 @@ export function update(canvas, faceCanvas, drawSmiley, triggerDeath, drawCooldow
     if(p.hp<=0){ p.broken=true; p.respawnTimer=PETAL_TYPES[p.type].respawnTime; continue; }
     p.cooldown = petalState==='attack' ? def.cooldownAtk : def.cooldownIdle;
     m.hitFlash = 10;
-
+  }
           if(p.type==='lightning'){
             const bx=m.x-player.x, by=m.y-player.y, bl=Math.hypot(bx,by)||1;
             m.x+=bx/bl*def.knockback; m.y+=by/bl*def.knockback;
