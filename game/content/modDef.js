070995ec2ec550cfee781686494f5fa7ea3ad53a
itsbloxonyt <phamhoanglam7749@gmail.com>
2026-04-26 19:42:11 +0700

    add modDef.js

---
+++ game/content/modDef.js
@@ -0,0 +1,6 @@
+export const MOB_TYPES = [
+    { color:'#ff6666', r:16, hp:20,  speed:0.9,  xp:5,  dmg:8,  name:'ladybug'  },
+    { color:'#ff9944', r:21, hp:45,  speed:0.65, xp:14, dmg:13, name:'bee'      },
+    { color:'#cc44ff', r:27, hp:90,  speed:0.5,  xp:28, dmg:20, name:'spider'   },
+    { color:'#ff2244', r:34, hp:170, speed:0.4,  xp:55, dmg:28, name:'centipede'},
+  ];
